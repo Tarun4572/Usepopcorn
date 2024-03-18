75308a19564909c5fc4001df1c7839869572a676
Tarun Pottigari <pottigarit@dnb.com>
2024-03-18 09:56:51 +0530

    console/log

--- src/App.js
+++ src/App.js
@@ -304,6 +304,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
           `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
+        console.log(data);
         setMovie(data);
         setIsLoading(false);
       }
